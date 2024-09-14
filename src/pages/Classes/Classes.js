4ac20fdc443fda02134443ffed24f005a48e513c
Ali Bakhshi <alibakhshi962002@gmail.com>
2024-09-14 12:53:36 +0330

    bazam commit

--- src/pages/Classes/Classes.js
+++ src/pages/Classes/Classes.js
@@ -17,47 +17,82 @@ function Classes() {
     slidesToScroll: 1,
     autoplay: false,
     autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1536,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ],
   };
 
   return (
     <>
-      <div className="bg">
-        <div>
-          <h3>کلاس های ویژه ی ما</h3>
-          <span>
+      <div className="bg-lightishBlue-600/20 py-2">
+        <div className="flex-center flex-col mb-4">
+          <h3 className="text-2xl lg:text-4xl font-MikhakWoff2one font-bold my-4">کلاس های ویژه ی ما</h3>
+          <span className="text-lg lg:text-xl px-4 text-gray-700 tracking-tighter text-center">
             دیگر برنامه های آموزشی ویژه ی مدرسه ی مونته سوری متانت شامل :
           </span>
         </div>
         <div className="slider-container">
           <Slider ref={(slider) => (sliderRef = slider)} {...settings}>
             <div>
-              <div>
-                <img src="images/Azmoon.jpg" alt="" />
+              <div className="mx-2">
+                <img src="images/Azmoon.jpg" className="w-full h-full rounded-xl" alt="" />
               </div>
             </div>
             <div>
-              <div>
-                <img src="images/falsafe2.jpg" alt="" />
+              <div className="mx-2">
+                <img src="images/child_falsafe.jpg" className="w-full h-full rounded-xl" alt="" />
               </div>
             </div>
             <div>
-              <div>
-                <img src="images/honareTajasomi2.jpg" alt="" />
+              <div className="mx-2">
+                <img src="images/honareTajasomi2.jpg" className="w-full h-full rounded-xl" alt="" />
               </div>
             </div>
             <div>
-              <div>
-                <img src="images/moshavere.jpg" alt="" />
+              <div className="mx-2">
+                <img src="images/moshavere.jpg" className="w-full h-full rounded-xl" alt="" />
               </div>
             </div>
             <div>
-              <div>
-                <img src="images/varzesh.jpg" alt="" />
+              <div className="mx-2">
+                <img src="images/varzesh.jpg" className="w-full h-full rounded-xl" alt="" />
               </div>
             </div>
             <div>
-              <div>
-                <img src="images/zabanEnglish.webp" alt="" />
+              <div className="mx-2">
+                <img src="images/zabanEnglishhh.jpg" className="w-full h-full rounded-xl" alt="" />
               </div>
             </div>
           </Slider>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -166,7 +166,7 @@ function Home() {
         </div>
       </div>
       {/* About Us */}
-      <div className="container">
+      <div className="container mb-12">
         <div className="flex flex-col lg:flex-row">
           <div className="relative flex-center my-16 lg:w-full">
             <div className="absolute w-full lg:w-[120%] h-[130%] sm:h-[122%] md:h-[118%] lg:h-[120%] xl:h-[105%] bg-pink-400 z-0 -top-14 lg:-top-12 xl:-top-4 -right-16 xs:-right-24 md:-right-36 rounded-tl-[70px]"></div>
