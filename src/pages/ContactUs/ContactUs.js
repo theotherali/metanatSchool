dba8f36ae6e9b3e25621340d222df5d1066281b7
Ali Bakhshi <alibakhshi962002@gmail.com>
2024-09-17 10:15:01 +0330

    images folder ignore

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -4,60 +4,53 @@ import { contactUsImage } from "../../datas";
 function ContactUs() {
   return (
     <div className="container">
-      <div className="flex-center space-y-3 lg:space-y-6 flex-col mb-3 xs:mb-8 lg:mb-10">
-        <h3 className="text-2xl lg:text-4xl font-MikhakWoff2one font-bold">
+      <div className="flex-center flex-col mb-12">
+        <h3 className="mb-3 xs:mb-8 lg:mb-10text-2xl lg:text-4xl font-MikhakWoff2one font-bold">
           در تماس باشیم
         </h3>
-      </div>
-      <div className="py-14 shadow-2xl">
-        <div className="flex w-full px-6 py-10">
+        <div className="w-[90%] h-[450px] bg-contactUs shadow-2xl rounded-3xl overflow-hidden">
+          <div className="h-full flex bg-gradient-to-r from-lightishBlue-800/40 via-emerald-800/30 to-black/70">
           <form
             action="#"
-            className="w-2/5 space-y-5 px-12 child:w-full child:bg-white child:shadow-md"
+            className="w-2/5 space-y-5 p-6"
           >
-            <div className="rounded-xl overflow-hidden p-4">
+            <div className="rounded-xl p-4 w-full shadow-white/15 backdrop-blur shadow-normal">
               <input
                 type="text"
-                className="w-full outline-none"
+                className="w-full text-white outline-none bg-transparent"
                 placeholder="نام و نام خانوادگی"
               />
             </div>
-            <div className="rounded-xl overflow-hidden p-4">
+            <div className="rounded-xl p-4 w-full shadow-white/15 backdrop-blur shadow-normal">
               <input
                 type="email"
-                className="w-full outline-none"
+                className="w-full text-white outline-none bg-transparent"
                 placeholder="ایمیل"
               />
             </div>
-            <div className="rounded-xl overflow-hidden p-4">
+            <div className="rounded-xl p-4 w-full shadow-white/15 backdrop-blur shadow-normal">
               <input
                 type="text"
-                className="w-full outline-none"
+                className="w-full text-white outline-none bg-transparent"
                 placeholder="موبایل"
               />
             </div>
-            <div className="rounded-xl overflow-hidden p-4">
+            <div className="rounded-xl p-4 w-full shadow-white/15 backdrop-blur shadow-normal">
               <input
                 type="text"
-                className="w-full outline-none"
+                className="w-full text-white outline-none bg-transparent"
                 placeholder="پیام ..."
               />
             </div>
-            <div>
-              <button type="submit" className="w-20">
+            <div className="shadow-none bg-slate-400">
+              <button type="submit" className="">
                 ارسال
               </button>
             </div>
           </form>
-          <div className="w-3/5 bg-[url('images/ContactUs.jpg')]">
+          <div className="w-3/5 flex-center flex-col text-center">
             <p>نظرات ارزشمند شما</p>
-            <div className="flex-center">
-              <img
-                src={contactUsImage}
-                className="w-[60%]"
-                alt="contactUsImage"
-              />
-            </div>
+          </div>
           </div>
         </div>
       </div>
